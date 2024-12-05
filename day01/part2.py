268d73b90f4bb8f5c301adaf112db76a8c1c0c33
ftagliaca <filippotagliacarne@gmail.com>
2024-12-05 17:17:14 +0100

    Day01

---
+++ day01/part2.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def parse_data(s: str) -> list[list[int]]:
+    return [[int(x) for x in r.split()] for r in s.split('\n') if r]
+
+def compute(s: str) -> int:
+    numbers = parse_data(s)
+    main, copy = list(zip(*numbers))
+
+    score = []
+    for item in main:
+        occ = copy.count(item)
+        score.append(item*occ)
+
+    return sum(score)
+
+
+INPUT_S = '''\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+'''
+EXPECTED = 31
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
